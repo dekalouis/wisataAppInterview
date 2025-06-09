591b6d2a6504b49ac48414a1279c323347371753
dekalouis <dekarebaudo@gmail.com>
2025-06-09 16:36:13 +0700

    fix: fixed final code

--- 03_Indonesian_Flag/test3.js
+++ 03_Indonesian_Flag/test3.js
@@ -1,17 +1,30 @@
-const width = 40;
-const height = 8;
+const width = 40 * 2;
+const height = 8 * 2;
 
-const topBorder = "-" + "-".repeat(width - 2) + "-";
+const topBorder = "-".repeat(width - 1);
 const botBorder = topBorder;
-// let rowflag = "";
-
 console.log(topBorder);
+
+//! FIX
+let innerWidth = width - 4;
+
 for (let i = 0; i < height; i++) {
   const fill = i < height / 2 ? "r" : "w";
 
-  const row = (fill + " ").repeat(width / 2 - 2).trim();
+  const charCount = Math.floor(innerWidth / 2);
+  // console.log(charCount + " per rownya");
+  const row = Array(charCount).fill(fill).join(" ");
+  // console.log(row + " rownya");
   console.log(`| ${row} |`);
 }
+//! FIX
+
+// for (let i = 0; i < height; i++) {
+//   const fill = i < height / 2 ? "r" : "w";
+
+//   const row = (fill + " ").repeat(width / 2 - 2).trim();
+//   console.log(`| ${row} |`);
+// }
 
 console.log(botBorder);
 
